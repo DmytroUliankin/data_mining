7cef1b06489d00929336896a6c7e183c5c441b83
Dmitry Uliankin <dimmich2001@gmail.com>
2021-09-24 07:53:21 +0300

    added graphics for word length distribution for each category and average word and message length

--- sms/main.py
+++ sms/main.py
@@ -12,8 +12,6 @@ regex = '[^A-Za-z ]'
 stop_words = stopwords.get_stopwords('english')
 
 porter = PorterStemmer()
-ham_without_stop_words = []
-spam_without_stop_words = []
 
 ham = {}
 spam = {}
@@ -28,14 +26,12 @@ with open('sms-spam-corpus.csv') as reader:
                 if new_word not in stop_words:
                     new_word = porter.stem(new_word)
                     ham[new_word] = ham.setdefault(new_word, 0) + 1
-                    ham_without_stop_words.append(row[1])
         elif re.match('spam', row[0]):
             for word in split:
                 new_word = re.sub(regex, '', word).lower()
                 if new_word not in stop_words:
                     new_word = porter.stem(new_word)
                     spam[new_word] = spam.setdefault(new_word, 0) + 1
-                    spam_without_stop_words.append(row[1])
 
 ham_sorted = dict(sorted(ham.items(), key=lambda x: x[1], reverse=True))
 spam_sorted = dict(sorted(spam.items(), key=lambda x: x[1], reverse=True))
@@ -67,5 +63,61 @@ spam_dict = {x[0]: int(x[1]) / len(spam_sample) for x in spam_list}
 pylab.subplot(2, 2, 2)
 pylab.bar(range(len(spam_dict)), height=spam_dict.values())
 pylab.xticks(numpy.arange(len(spam_dict)), spam_dict.keys(), rotation='vertical')
+
+ham_words = {}
+for k in ham_sorted.keys():
+    ham_words[len(k)] = ham_words.setdefault(len(k), 0) + 1
+
+spam_words = {}
+for k in spam_sorted.keys():
+    spam_words[len(k)] = spam_words.setdefault(len(k), 0) + 1
+
+ham_length = 0
+ham_sum = 0
+for key, value in ham_words.items():
+    ham_length += int(key) * int(value)
+    ham_sum += value
+
+spam_length = 0
+spam_sum = 0
+for key, value in spam_words.items():
+    spam_length += int(key) * int(value)
+    spam_sum += value
+
+sms_length = ham_length + spam_length
+sms_sum = ham_sum + spam_sum
+average = sms_length / sms_sum
+
+pylab.subplot(2, 2, 3)
+pylab.bar(ham_words.keys(), height=ham_words.values())
+
+pylab.subplot(2, 2, 3)
+pylab.bar(spam_words.keys(), height=spam_words.values())
+
+pylab.subplot(2, 2, 3)
+pylab.bar(average, height=1300, color='red')
+
+ham_messages = {}
+spam_messages = {}
+total_len = 0
+total_average = 0
+with open('sms-spam-corpus.csv', 'r') as r:
+    csv_reader = csv.reader(r)
+    for row in csv_reader:
+        if re.match('ham', row[0]):
+            ham_messages[len(row[1])] = ham_messages.setdefault(len(row[1]), 0) + 1
+        elif re.match('spam', row[0]):
+            spam_messages[len(row[1])] = spam_messages.setdefault(len(row[1]), 0) + 1
+        total_len += len(row[1])
+    total_average = total_len / csv_reader.line_num
+
+pylab.subplot(2, 2, 4)
+pylab.bar(ham_messages.keys(), height=ham_messages.values())
+
+pylab.subplot(2, 2, 4)
+pylab.bar(spam_messages.keys(), height=spam_messages.values())
+
+pylab.subplot(2, 2, 4)
+pylab.bar(total_average, height=80, color='red')
 
 pylab.show()
